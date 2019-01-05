0294bd4b2bbb3181432ed37e26fcb3fdf069162e
Ivan Kovachev <ivan.kovachev777@gmail.com>
2019-01-05 14:08:06 +0200

    home page

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,89 +1,65 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-
-	<style type="text/css">
-
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+  <script type="text/javascript" src="jquery/dist/jquery.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+  <script src="semantic/dist/semantic.min.js"></script>
 </head>
-<body>
-
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
-
-</body>
+	<body>
+		<div class="ui massive menu">
+		  <a class="active item">
+		    Home
+		  </a>
+		  <a class="item">
+		    Messages
+		  </a>
+		  <div class="right menu">
+		    <div class="ui dropdown item">
+		      Language <i class="dropdown icon"></i>
+		      <div class="menu">
+		        <a class="item">English</a>
+		        <a class="item">Russian</a>
+		        <a class="item">Spanish</a>
+		      </div>
+		    </div>
+		    <div class="item">
+		        <div class="ui primary button">Sign Up</div>
+		    </div>
+		  </div>
+		</div>
+		<div class="ui menu">
+		  <div class="ui category search item">
+		    <div class="ui transparent icon input">
+		      <input class="prompt" type="text" placeholder="Search books...">
+		      <i class="search link icon"></i>
+		    </div>
+		    <div class="results"></div>
+		  </div>
+		</div>
+		<div class="ui grid">
+		  <div class="four wide column">
+		    <div class="ui vertical fluid tabular menu">
+		      <a class="active item">
+		        Bio
+		      </a>
+		      <a class="item">
+		        Pics
+		      </a>
+		      <a class="item">
+		        Companies
+		      </a>
+		      <a class="item">
+		        Links
+		      </a>
+		    </div>
+		  </div>
+		  <div class="twelve wide stretched column">
+		    <div class="ui segment">
+		      This is an stretched grid column. This segment will always match the tab height
+		    </div>
+		  </div>
+		</div>
+	</body>
 </html>
